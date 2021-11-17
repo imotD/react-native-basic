af38ecf9e2041ef4f3721120baefaf252590cd9d
Tommy Alhamra <tommy.alhamra@gmail.com>
2021-11-17 12:53:10 +0700

    add img and textinput

--- App.js
+++ App.js
@@ -1,19 +1,32 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Image, Text, TextInput, View} from 'react-native';
 
 const App = () => {
   return (
     <View>
-      <View style={{width: 80, height: 100, backgroundColor: 'red'}}></View>
-      <Text>Ini Adalah App 1</Text>
-      <Text>Ini Adalah App 2</Text>
+      <View
+        style={{width: 100, height: 100, backgroundColor: '#FF5F7E'}}></View>
+      <Text>Ini Adalah App Circle</Text>
+      <Photo />
       <Homes />
+      <TextInput style={{borderWidth: 1}} />
     </View>
   );
 };
 
 const Homes = () => {
-  return <Text>Ini Adalah Homes</Text>;
+  return <Text>Ini Adalah Text dari beda komponen</Text>;
+};
+
+const Photo = () => {
+  return (
+    <Image
+      source={{
+        uri: 'https://api.openpay.mx/barcode/185017437691737?width=1&height=100',
+      }}
+      style={{height: 100, width: 100}}
+    />
+  );
 };
 
 export default App;
